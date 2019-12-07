fb584d9d8d478bbd121692d27139ff05da46daad
OmriRaz <43149084+OmriRaz@users.noreply.github.com>
2019-12-07 18:27:35 +0200

    Add files via upload
    This code sends a string over serial to another arduino.

---
+++ Challenge-ReadSerial-Master.ino
@@ -0,0 +1,26 @@
+
+      //============================//
+     //                            //
+    // 2019 Ninja Challenge Intel //
+   //  Read Serial, Read!        //
+  // Code Written By: Omri Raz  //
+ //                            //
+//============================//
+
+//THIS CODE IS FOR THE MASTER (SENDING) ARDUINO
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial MASTER(2,3);
+String SecretCode = "Intel Ninja Challenge";
+void setup() {
+  Serial.begin(9600);
+  MASTER.begin(9600);
+
+}
+
+void loop() {
+  MASTER.print(SecretCode);
+  delay(500);
+
+}

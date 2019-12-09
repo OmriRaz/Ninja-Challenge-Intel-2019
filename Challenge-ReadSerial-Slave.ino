3418ae541ff69b982e303dd52b99e4d7f203da0c
OmriRaz <43149084+OmriRaz@users.noreply.github.com>
2019-12-09 12:37:59 +0200

    Add files via upload

---
+++ Challenge-ReadSerial-Slave.ino
@@ -0,0 +1,16 @@
+#include<SoftwareSerial.h>
+SoftwareSerial SLAVE(2,3);
+String input = "0";
+
+void setup() {
+  SLAVE.begin(9600);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  while(SLAVE.available() > 0) {
+    input = SLAVE.read();
+    Serial.println("Recieved code is: " + input);
+  }
+}
